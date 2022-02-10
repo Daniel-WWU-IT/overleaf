2f53c347ee46993079c5244e4628442b8aeef5dd
Daniel Müller <daniel.mueller@uni-muenster.de>
2022-02-10 13:27:18 +0100

    Refactoring

--- runit/remote-api-server/server.py
+++ runit/remote-api-server/server.py
@@ -3,26 +3,30 @@ from flask import *
 from urllib.parse import urlparse, parse_qs
 from html import escape
 
+
 app = Flask(__name__)
+client = requests.session()
+
+
+def extract_csrf_token(link):
+    response = client.get(link)
+    regex = r"<input\s*name=[\"']_csrf[\"']\s*type=[\"']hidden[\"']\s*value=[\"']([a-zA-z0-9-]*)[\"'].*>"
+    matches = re.finditer(regex, response.text, re.MULTILINE|re.DOTALL)
+    return next(matches).group(1).strip()
+
 
 def set_password(link, password):
     url = urlparse(link)
     params = parse_qs(url.query)
-
-    client = requests.session()
+    redirectUrl = url._replace(netloc='localhost')._replace(scheme='http')
 
     # Get CSRF token from activation page
-    redirectUrl = url._replace(netloc='localhost')._replace(scheme='http')
-    response = client.get(redirectUrl.geturl())
-
-    regex = r"<input\s*name=[\"']_csrf[\"']\s*type=[\"']hidden[\"']\s*value=[\"']([a-zA-z0-9-]*)[\"']>"
-    matches = re.finditer(regex, response.text, re.MULTILINE|re.DOTALL)
-    csrf = next(matches).group(1).strip()
+    csrf = extract_csrf_token(redirectUrl.geturl())
 
     # Perform POST request to set the password
     client.post('http://localhost/user/password/set', data={'_csrf': csrf, 'passwordResetToken': params['token'][0], 'password': password})
 
-@app.route("/")
+
 def create_user():
     email = request.args.get('email', '')
     if email == '':
@@ -46,3 +50,13 @@ def create_user():
             return '<a href="' + link + '">' + escape(link) + '</a>'
     except:
         abort(503)
+
+
+@app.route("/")
+def regsvc():
+    action = request.args.get('action', 'create')
+
+    if (action.casefold() == 'create'):
+        return create_user()
+    else:
+        abort(404)
