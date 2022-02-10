bbf7695d54b0165ef42ae34b2546b878896ea7bc
Daniel Müller <daniel.mueller@uni-muenster.de>
2022-02-10 15:39:47 +0100

    Add login through API

--- runit/remote-api-server/server.py
+++ runit/remote-api-server/server.py
@@ -8,23 +8,29 @@ app = Flask(__name__)
 client = requests.session()
 
 
-def extract_csrf_token(link):
+def extract_auth_tokens(link):
     response = client.get(link)
+
     regex = r"<input\s*name=[\"']_csrf[\"']\s*type=[\"']hidden[\"']\s*value=[\"']([a-zA-z0-9-]*)[\"'].*>"
     matches = re.finditer(regex, response.text, re.MULTILINE|re.DOTALL)
-    return next(matches).group(1).strip()
+    return next(matches).group(1).strip(), response.headers, response.cookies
 
 
 def set_password(link, password):
-    url = urlparse(link)
-    params = parse_qs(url.query)
-    redirectUrl = url._replace(netloc='localhost')._replace(scheme='http')
-
-    # Get CSRF token from activation page
-    csrf = extract_csrf_token(redirectUrl.geturl())
+    # Get auth tokens from activation page
+    csrf, h, c = extract_auth_tokens(link)
 
     # Perform POST request to set the password
-    client.post('http://localhost/user/password/set', data={'_csrf': csrf, 'passwordResetToken': params['token'][0], 'password': password})
+    params = parse_qs(urlparse(link).query)
+    return client.post('http://localhost/user/password/set', data={'_csrf': csrf, 'passwordResetToken': params['token'][0], 'password': password})
+
+
+def perform_login(email, password):
+    # Get auth tokens from login page
+    csrf, h, c = extract_auth_tokens('http://localhost/login')
+
+    # Perform POST request to login the user
+    return client.post('http://localhost/login', data={'_csrf': csrf, 'email': email, 'password': password})
 
 
 def create_user():
@@ -44,6 +50,7 @@ def create_user():
         link = next(matches).group(1).strip()
 
         if password != '':
+            link = urlparse(link)._replace(netloc='localhost')._replace(scheme='http').geturl()
             set_password(link, password)
             return redirect('/login', code=302)
         else:
@@ -52,11 +59,37 @@ def create_user():
         abort(503)
 
 
+def login():
+    email = request.args.get('email', '')
+    password = request.args.get('password', '')
+    if email == '' or password == '':
+        abort(400)
+
+    try:
+        login = perform_login(email, password)
+
+        response = make_response(redirect('/project', code=302))
+        for key in login.headers:
+            response.headers[key] = login.headers[key]
+        cookies = login.cookies.get_dict('localhost.local')
+        for key in cookies:
+            response.set_cookie(key, cookies[key])
+
+        return response
+    except:
+        abort(503)
+
+
 @app.route("/")
 def regsvc():
-    action = request.args.get('action', 'create')
+    action = request.args.get('action', 'create-and-login')
 
-    if (action.casefold() == 'create'):
+    if action.casefold() == 'create':
         return create_user()
+    elif action.casefold() == 'login':
+        return login()
+    elif action.casefold() == 'create-and-login':
+        create_user()
+        return login()
     else:
         abort(404)
