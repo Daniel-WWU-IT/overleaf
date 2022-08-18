d67615977b43beabe9bf3e6b2375f1afabd33310
Daniel Müller <daniel.mueller@uni-muenster.de>
2022-08-18 13:55:28 +0200

    Proxy handling, URL rewriting

--- runit/remote-api-server/server.py
+++ runit/remote-api-server/server.py
@@ -6,7 +6,6 @@ from html import escape
 
 
 app = Flask(__name__)
-client = requests.session()
 
 
 # Cryptography functions
@@ -31,13 +30,18 @@ def _data_response(data={}, data_key=''):
     return resp
 
 
-def _error(msg, code=500):
+def _error(client, msg, code=500):
     app.logger.error(msg)
+    client.close()
     abort(code)
 
 
+def _resolveURL(path):
+    return os.getenv('SHARELATEX_SITE_URL', '').rstrip('/') + '/' + path.lstrip('/')
+
+
 # Main functions
-def extract_auth_tokens(link):
+def extract_auth_tokens(client, link):
     response = client.get(link)
 
     regex = r"<input\s*name=[\"']_csrf[\"']\s*type=[\"']hidden[\"']\s*value=[\"']([a-zA-z0-9-]*)[\"'].*>"
@@ -45,32 +49,32 @@ def extract_auth_tokens(link):
     return next(matches).group(1).strip(), response.headers, response.cookies
 
 
-def set_password(link, password):
+def set_password(client, link, password):
     # Get auth tokens from activation page
-    csrf, h, c = extract_auth_tokens(link)
+    csrf, h, c = extract_auth_tokens(client, link)
 
     # Perform POST request to set the password
     params = parse_qs(urlparse(link).query)
-    return client.post('http://localhost/user/password/set', data={'_csrf': csrf, 'passwordResetToken': params['token'][0], 'password': password})
+    return client.post(_resolveURL('/user/password/set'), data={'_csrf': csrf, 'passwordResetToken': params['token'][0], 'password': password})
 
 
-def perform_login(email, password):
+def perform_login(client, email, password):
     # Get auth tokens from login page
-    csrf, h, c = extract_auth_tokens('http://localhost/login')
+    csrf, h, c = extract_auth_tokens(client, _resolveURL('/login'))
 
     # Perform POST request to login the user
-    return client.post('http://localhost/login', data={'_csrf': csrf, 'email': email, 'password': password})
+    return client.post(_resolveURL('/login'), data={'_csrf': csrf, 'email': email, 'password': password})
 
 
-def create_user():
+def create_user(client):
     email = request.args.get('email', '')
     if email == '':
-        _error('Email address missing', 400)
+        _error(client, 'Email address missing', 400)
     password = request.args.get('password', '')
 
     result = subprocess.run(['grunt', 'user:create', '--email=' + email], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if result.returncode != 0:
-        _error('Creating the user account failed (process error)')
+        _error(client, 'Creating the user account failed (process error)')
 
     try:
         output = str(result.stdout)
@@ -79,23 +83,22 @@ def create_user():
         link = next(matches).group(1).strip()
 
         if password != '':
-            link = urlparse(link)._replace(netloc='localhost')._replace(scheme='http').geturl()
-            set_password(link, password)
+            set_password(client, link, password)
             return _data_response()
         else:
           return _data_response({'url': link})
     except BaseException as e:
-        _error('Creating a user resulted in an exception: ' + str(e))
+        _error(client, 'Creating a user resulted in an exception: ' + str(e))
 
 
-def login(data_enc_key):
+def login(client, data_enc_key):
     email = request.args.get('email', '')
     password = request.args.get('password', '')
     if email == '' or password == '':
-        _error('Login: Email or password missing', 400)
+        _error(client, 'Login: Email or password missing', 400)
 
     try:
-        login = perform_login(email, password)
+        login = perform_login(client, email, password)
 
         # Get all Sharelatex-specific headers and cookies
         data = {'headers': {}, 'cookies': {}}
@@ -103,25 +106,25 @@ def login(data_enc_key):
             if 'sharelatex' in key.casefold():
                 data['headers'][key] = login.headers[key]
 
-        cookies = login.cookies.get_dict('localhost.local')
+        cookies = login.cookies.get_dict(urlparse(_resolveURL('')).netloc)
         for key in cookies:
             if 'sharelatex' in key.casefold():
                 data['cookies'][key] = cookies[key]
 
         return _data_response(data, data_enc_key)
     except BaseException as e:
-        _error('Logging in a user resulted in an exception: ' + str(e))
+        _error(client, 'Logging in a user resulted in an exception: ' + str(e))
 
 
-def open_projects(data_enc_key):
+def open_projects(client, data_enc_key):
     data = request.args.get('data', '')
     if data == '':
-        _error('Open: Data missing', 400)
+        _error(client, 'Open: Data missing', 400)
     data = _decrypt_data(data, data_enc_key)
 
     try:
         req_data = json.loads(data)
-        response = make_response(redirect(os.getenv('SHARELATEX_SITE_URL', '').rstrip('/') + '/project', code=302))
+        response = make_response(redirect(_resolveURL('/project'), code=302))
 
         # Copy all specified headers and cookies into the request
         for key in req_data['headers']:
@@ -132,18 +135,18 @@ def open_projects(data_enc_key):
 
         return response
     except BaseException as e:
-        _error('Opening the projects resulted in an exception: ' + str(e))
+        _error(client, 'Opening the projects resulted in an exception: ' + str(e))
 
 
 # API key handling
 def verify_api_key():
     api_key = os.getenv('REMOTE_API_KEY', '')
     if api_key == '':
-        _error('No API key set', 500)
+        _error(client, 'No API key set', 500)
 
     req_key = request.args.get('apikey', '')
     if req_key != api_key:
-        _error('Invalid API key specified', 503)
+        _error(client, 'Invalid API key specified', 503)
 
 
 # App routing
@@ -157,30 +160,42 @@ def verify_client():
         if fnmatch.fnmatchcase(request.remote_addr.casefold(), allowed_client.casefold()):
             break
     else:
-        _error(f'Request from {request.remote_addr} is not allowed', 503)
+        _error(client, f'Request from {request.remote_addr} is not allowed', 503)
 
 
 @app.route("/")
 def regsvc():
+    # Requests session
+    client = requests.Session()
+
+    # Get all extended headers
+    for header in request.headers:
+        if header[0].startswith('X-Forwarded'):
+            client.headers[header[0]] = header[1]
+
     # Get the key used to encrypt login data; specifying this is mandatory
     data_enc_key = os.getenv('REMOTE_API_DATA_KEY', '')
     if data_enc_key == '':
-        _error('No data key set', 500)
+        _error(client, 'No data key set', 500)
     data_enc_key = base64.b64encode(data_enc_key.encode()) # Required by Fernet
 
     action = request.args.get('action', 'create-and-login')
+    result = None
     if action.casefold() == 'create':
         verify_api_key()
-        return create_user()
+        result = create_user(client)
     elif action.casefold() == 'login':
         verify_api_key()
-        return login(data_enc_key)
+        result = login(client, data_enc_key)
     elif action.casefold() == 'create-and-login':
         verify_api_key()
-        create_user()
-        return login(data_enc_key)
+        create_user(client)
+        result = login(client, data_enc_key)
     elif action.casefold() == 'open-projects':
         # This EP is public
-        return open_projects(data_enc_key)
+        result = open_projects(client, data_enc_key)
     else:
-        _error('Unknown action', 404)
+        _error(client, 'Unknown action', 404)
+
+    client.close()
+    return result
