57b008c09e0052922690e24cae39ab9ff7e19953
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-02-20 11:48:54 +0100

    add version to footer

--- server-ce/runit/ui-proxy/js/modify-project.js
+++ server-ce/runit/ui-proxy/js/modify-project.js
@@ -20,7 +20,13 @@ function addVersionToFooter() {
     if (rowElem.length > 0) {
       const footerItems = rowElem.find("ul");
       if (footerItems.length > 0) {
+        const leftEntryElem = footerItems.first();
         const rightEntryElem = footerItems.last();
+
+        leftEntryElem.removeClass("col-lg-9");
+        leftEntryElem.addClass("col-lg-6");
+        rightEntryElem.removeClass("col-lg-9");
+        rightEntryElem.addClass("col-lg-6");
 
         if (rightEntryElem.find("li:contains(version)").length === 0) {
           rightEntryElem.find("li")?.html(`<strong>Overleaf version:</strong> ${OVERLEAF_VERSION} <strong>Integration version:</strong> ${INTEGRATION_VERSION}`);
