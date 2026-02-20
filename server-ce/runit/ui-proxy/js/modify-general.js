b57729d850b75b1422d23e12a2b39525c90cf812
Daniel Müller <daniel.mueller@uni-muenster.de>
2026-02-20 10:12:28 +0100

    further project page mods

--- server-ce/runit/ui-proxy/js/modify-general.js
+++ server-ce/runit/ui-proxy/js/modify-general.js
@@ -6,6 +6,11 @@ function adjustFooterItems() {
     footerLnk.attr("href", "https://www.overleaf.com")
     footerLnk.attr("target", "_blank")
   }
+
+  const navMainDiv = $("div[class='project-ds-nav-main']");
+  if (navMainDiv.length > 0) {
+    navMainDiv.css("min-height", "calc(100vh - 7.5rem)");
+  }
 }
 
 // Entry hook
